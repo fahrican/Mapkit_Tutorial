48ed9b51de45b428ccb3d4f6391d9c9725154712
Fahri MacBook <fahri-can@web.de>
2018-05-29 00:28:47 +0200

    First use of MapKit

--- Mapkit_Tutorial/ViewController.swift
+++ Mapkit_Tutorial/ViewController.swift
@@ -7,12 +7,33 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var map: MKMapView!
+    let locationManager = CLLocationManager()
+    
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+       super.viewDidLoad()
+//        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+//        locationManager.requestLocation()
+        let location = CLLocationCoordinate2D(latitude: 48.239283, longitude: 16.377324)
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegion(center: location, span: span)
+        map.setRegion(region, animated: true)
+        
+        map.showsUserLocation = true
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = "Technikum"
+        annotation.subtitle = "Wien"
+        map.addAnnotation(annotation)
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,4 +43,22 @@ class ViewController: UIViewController {
 
 
 }
+
+//extension ViewController : CLLocationManagerDelegate {
+//    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+//        if status == .authorizedWhenInUse {
+//            locationManager.requestLocation()
+//        }
+//    }
+//
+//    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        if let location = locations.first {
+//            print("location:: (location)")
+//        }
+//    }
+//
+//    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+//        print("error:: (error)")
+//    }
+//}
 
